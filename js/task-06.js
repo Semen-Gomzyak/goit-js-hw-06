8ed3cfe21f8b3d5930a51dbf98d5a26ccff8eb08
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-08-06 00:15:50 +0200

    change task 6 and 8

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,11 @@ const inputEl = document.querySelector('#validation-input');
 
 
 inputEl.addEventListener("blur", (event) => {
-    
+
  if (Number(inputEl.value.length) === Number(inputEl.dataset.length)) {
-    
+     if (inputEl.classList.contains('invalid')) {
+        inputEl.classList.remove('invalid')
+    }
      return inputEl.classList.add('valid')
      
     }
--- js/task-08.js
+++ js/task-08.js
@@ -3,17 +3,22 @@ const formEl = document.querySelector(".login-form");
 formEl.addEventListener('submit', handleSubmit);
  
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
     
   const {
     elements: { email, password }
   } = event.currentTarget;
 
+  const user = {
+    Email: email.value,
+    Password: password.value
+  }
+
   if (email.value === "" || password.value === "") {
     return window.alert("Пожалуйста, заполните все поля!");
   }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  
+  console.log(user);
   event.currentTarget.reset();
 }
 
